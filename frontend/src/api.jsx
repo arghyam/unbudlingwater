198b4a09bed164d2729bb0be0c789b6de66b03e1
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-01-27 23:55:18 +0530

    put api url to env vars

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,4 +1,4 @@
-const BASE_URL = "https://appwrite.opencoursehub.online/api/v1";
+const BASE_URL = import.meta.env.VITE_SERVER_URL;
 // const BASE_URL = '/api/v1';
 
 async function fetchLocationByName(name) {
